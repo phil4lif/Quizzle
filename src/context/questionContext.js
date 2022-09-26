ab2dac4f68bbeb6be50b55107ca9cb4e26b90bee
Phil <philipjcowan1@gmail.com>
2022-09-26 10:30:57 -0700

    update to use lambda endpoint

--- src/context/questionContext.js
+++ src/context/questionContext.js
@@ -12,10 +12,10 @@ export const QuestionProvider = ({children}) => {
   const getQotd = async () => {
     setQotdLoading(true);
     try {
-      const response = await api.get('/api/card/qotd')
-      // const response = await axios.get('https://jxezbaggvb.execute-api.us-east-1.amazonaws.com/dev')
+      // const response = await api.get('/api/card/qotd')
+      const response = await axios.get('https://jxezbaggvb.execute-api.us-east-1.amazonaws.com/dev')
       if (response.data) {
-        setQotd(response.data)
+        setQotd(response.data.qotd)
         setQotdLoading(false);
       }
     } catch (error) {
